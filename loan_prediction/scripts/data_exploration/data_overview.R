2973870a9818b60b39db973073ba6d0db8129750
RhysKemp <rhyskemp15@gmail.com>
2024-12-20 22:07:24 +0000

    Update univariate_visualisation.R
    Finish univariate distribution analysis with KDE for all numerical categories.
    
    Added Boxplots for numerical categories
    
    Begin work on univariate distribution analysis for categorical columns
    
    Begin refactor to assist in distribution analysis:
    - created bar chart helper function
    - begin working on the distribution function
    - Requires a pie chart helper function

--- loan_prediction/scripts/data_exploration/data_overview.R
+++ loan_prediction/scripts/data_exploration/data_overview.R
@@ -66,13 +66,6 @@ print(summary(data))
 cat("\nData Types of Columns:\n")
 print(sapply(data, class))
 
-# =====================================
-# Unique Values in each column
-# =====================================
-
-cat("\nUnique Values in each column:\n")
-print()
-
 
 # =====================================
 # Notes for Next Steps
--- loan_prediction/scripts/data_exploration/visualisation/univariate_visualisation.R
+++ loan_prediction/scripts/data_exploration/visualisation/univariate_visualisation.R
@@ -14,6 +14,9 @@ library(ggplot2)
 library(RColorBrewer)
 library(dplyr)
 library(gridExtra)
+library(scales)
+
+
 
 # =====================================
 # Define File Paths
@@ -24,15 +27,82 @@ project_dir <- getwd()
 # Input files:
 input_path <- file.path(project_dir, "data", "raw", "loan_data.csv")
 
-# Output files:
-output_path <- file.path(project_dir, "[output_folder]", "[output_file_name.csv]")
-
 # =====================================
 # Load Dataset
 # =====================================
 
 data <- read.csv(input_path)
 cat("Dataset loaded from:", input_path, "\n")
+
+# =====================================
+# Variables for graph palettes
+# =====================================
+
+bar_palette_one <- "Dark2"
+pie_palette_one <- "Dark2"
+box_palette_one <- "Dark2"
+histogram_palette_one <- "Dark2"
+
+# =====================================
+#  Graph Helper Functions
+# =====================================
+
+create_bar_plot <- function(data, x_column, fill_column = NULL,
+                            x_labels = NULL, plot_title = "Bar Plot",
+                            x_axis_title = "X-Axis", y_axis_title = "Frequency",
+                            fill_pallette = bar_palette_one ) {
+  
+  # Convert columns to factors if necessary
+  data[[x_column]] <- factor(data[[x_column]])
+  if (!is.null(fill_column)) {
+    data[[fill_column]] <- factor(data[[fill_column]])
+  }
+  
+  # fill defaults to x_column
+  if (is.null(fill_column)) {
+    fill_column <- x_column
+  }
+  
+  # if no labels use unique values in the column
+  if (is.null(x_labels)) {
+    x_labels <- unique(data[[x_column]])
+  }
+  
+  # Create bar plot
+  bar_plot <- ggplot(data, aes_string(x = x_column, fill = fill_column)) +
+    geom_bar(color = "black", alpha = 1, stat = "count") +
+    geom_text(
+      aes(label = ..count..), 
+      stat = "count", 
+      vjust = -0.5, 
+      color = "black", 
+      size = 5
+    ) +
+    scale_x_discrete(labels = x_labels) +
+    scale_fill_brewer(
+      palette = fill_pallette,
+      labels = x_labels
+      ) +
+    labs(
+      title = plot_title,
+      x = x_axis_title,
+      y = y_axis_title
+    ) +
+    theme_classic() +
+    theme(
+      axis.text.x = element_text(angle = 0, hjust = 0.5, face = "bold"),
+      plot.title = element_text(color = "black", size = 16, face = "bold"),
+      legend.background = element_rect(color = "black", linewidth = 1),
+      legend.title = element_text(size = 12, face = "bold", color = "black")
+    ) +
+    guides(fill = guide_legend(title = fill_column))
+  
+  return(bar_plot)
+}
+
+# TODO:
+#     - add pie chart helper function
+
 
 # =====================================
 # Visualise Loan Status
@@ -48,11 +118,63 @@ loan_status_df <- data.frame(
   percentage = round(as.numeric(loan_status_percent), 1)
 )
 
-head(loan_status_df)
+b_plot <- create_bar_plot(
+  data = data, 
+  x_column = "loan_status", 
+  x_labels = c("Rejected (0)", "Approved (1)"), 
+  plot_title = "Loan Status Distribution", 
+  x_axis_title = "Loan Status", 
+  y_axis_title = "Frequency"
+)
+
+b_plot <- b_plot +
+  guides(fill = guide_legend(
+    title = "Loan Status", 
+    title.position = "top", 
+    title.theme = element_text(size = 12, face = "bold", color = "black"),
+    label.theme = element_text(size = 10, face = "bold", color = "black")
+  ))
+
+print(b_plot)
+
+
+# TODO:
+#     - Fix y_interval param
+#     - add pie chart for percentage distribution
+#     - refactor all distribution code to utilise
+plot_distribution_column <- function(data, column, y_interval = NULL) {
+  
+  # Column handling
+  column_title <- gsub("_", " ", column)
+  
+  
+  # Create bar plot
+  bar_title <- paste(column_title, "Distribution")
+  bar_plot <- create_bar_plot(
+    data = data,
+    column,
+    x_axis_title = column_title,
+    plot_title = bar_title,
+    )
+  
+  
+  bar_plot <- bar_plot +
+    guides(fill = guide_legend(
+      title = "Loan Status", 
+      title.position = "top", 
+      title.theme = element_text(size = 12, face = "bold", color = "black"),
+      label.theme = element_text(size = 10, face = "bold", color = "black")
+    ))
+  
+  print(bar_plot)
+  
+}
+
+plot_distribution_column(data, "loan_amount", 5000)
 
 # Plot Bar chart
 bar_plot <- ggplot(data, aes(x = factor(loan_status), fill = factor(loan_status))) +
-  geom_bar(color = "black", alpha = 0.7, stat = "count") +
+  geom_bar(color = "black", alpha = 1, stat = "count") +
   geom_text(
     aes(label = ..count..), 
     stat = "count", 
@@ -60,10 +182,11 @@ bar_plot <- ggplot(data, aes(x = factor(loan_status), fill = factor(loan_status)
     color = "black", 
     size = 5
   ) +
+  # Aesthetics and theme
   scale_x_discrete(labels = c("Rejected", "Approved")) +
   scale_y_continuous(breaks = seq(0, max(table(data$loan_status)), by = 5000)) +
   scale_fill_brewer(
-    palette = "Dark2",
+    palette = bar_palette_one,
     labels = c("Rejected (0)", "Approved (1)")
     ) +
   labs(
@@ -75,8 +198,8 @@ bar_plot <- ggplot(data, aes(x = factor(loan_status), fill = factor(loan_status)
   theme(
     axis.text.x = element_text(angle = 0, hjust = 0.5, face = "bold"),
     plot.title = element_text(color = "black", size = 16, face = "bold"),
-    legend.background = element_rect(color = "black", size = 1),
-    legend.title = element_text(size = 12, face = "bold", color = "black")  # Set legend title style
+    legend.background = element_rect(color = "black", linewidth = 1),
+    legend.title = element_text(size = 12, face = "bold", color = "black")
   ) +
   guides(fill = guide_legend(title = "Loan Status"))
 
@@ -90,7 +213,10 @@ pie_plot <- ggplot(loan_status_df, aes(x = "", y = percentage, fill = loan_statu
     color = "white", 
     size = 5
   ) +
-  scale_fill_brewer(palette = "Dark2", labels = c("Rejected (0)", "Approved (1)")) +
+  scale_fill_brewer(
+    palette = pie_palette_one,
+    labels = c("Rejected (0)", "Approved (1)")
+    ) +
   labs(
     title = "Loan Status Percentage Distribution"
   ) +
@@ -104,9 +230,16 @@ pie_plot <- ggplot(loan_status_df, aes(x = "", y = percentage, fill = loan_statu
 grid.arrange(bar_plot, pie_plot, ncol = 2)
 
 # =====================================
-# Univarate Analysis for Numeric Columns
+# Univariate Analysis for Numeric Columns
+# Distribution with KDE
 # =====================================
 
+# List of numerical columns for analysis
+numerical_columns_to_analyse <- c('person_age', 'person_income', 'person_emp_exp', 'loan_amnt', 
+                        'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length', 
+                        'credit_score')
+
+# Function for univariate analysis on numeric columns
 univariate_analysis <- function(data, columns) {
   plot_list <- list()
   
@@ -114,36 +247,30 @@ univariate_analysis <- function(data, columns) {
   for (i in seq_along(columns)) {
     column <- columns[i]
     
-    # Create histogram with density curve 
-    # TODO:
-    #     - Fix Scaling issues
-    #     - Fix aesthetics 
-    #     - Understand more thoroughly
+    # Create histogram with density probability curve 
     p <- ggplot(data, aes_string(x = column)) + 
       geom_histogram(
         aes(y = ..density..),
         bins = 30,
-        fill = brewer.pal(3, "Blues")[2],
-        color = "black", alpha = 0.7
+        fill = brewer.pal(3, histogram_palette_one)[1],
+        color = "black",
         ) + 
-      geom_density(
-        color = brewer.pal(3, "Blues")[3],
+      geom_density( # KDE
+        color = brewer.pal(3, histogram_palette_one)[2],
         size = 1
-        ) + 
+      ) + 
       labs(
         title = paste(gsub("_", " ", column), "Distribution with KDE"),
         x = gsub("_", " ", column),
         y = "Density"
         ) +
-      scale_fill_brewer(palette = "Set1") +
-      scale_color_brewer(palette = "Set1") +
+      # Aesthetics and theme
+      scale_y_continuous(labels = comma) +
+      scale_x_continuous(labels = comma, breaks = scales::pretty_breaks(n = 10)) +
       theme_classic() +
       theme(
-        plot.title = element_text(color = "black", size = 14, face = "bold"),
-        axis.text.x = element_text(angle = 0, hjust = 0.5),
-        axis.text.y = element_text(color = "black"),
-        axis.title = element_text(size = 12),
-        plot.margin = unit(c(1, 1, 1, 1), "lines")  # Adjust margins for better spacing
+        panel.grid.major = element_line(color = "gray90", size = 0.5),
+        panel.grid.minor = element_line(color = "gray90", size = 0.2)
       )
     
     # Add plot to list
@@ -153,19 +280,94 @@ univariate_analysis <- function(data, columns) {
   grid.arrange(grobs = plot_list, ncol = 2)
 }
 
-columns_to_analyze <- c('person_age', 'person_income', 'person_emp_exp', 'loan_amnt', 
-                        'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length', 
-                        'credit_score')
 
-univariate_analysis(data, columns_to_analyze)
+univariate_analysis(data, numerical_columns_to_analyse)
 
 # =====================================
-# 
+# Univariate Analysis for Numeric Columns
+# Boxplot
 # =====================================
 
+# Function for univariate analysis on numeric columns
+univariate_analysis_boxplot <- function(data, column) {
+  # boxplot
+  p <- ggplot(data, aes_string(x = column)) +
+    stat_boxplot(geom ="errorbar", width = 0.5) + # whisker endcaps
+    geom_boxplot(
+      fill = brewer.pal(3, "Set3")[2],
+      color = "black",
+      outlier.color = "black",
+      outlier.shape = 16,
+      outlier.size = 2,
+    ) +
+    labs(
+      title = paste(gsub("_", " ", column), "Boxplot"),
+      x = gsub("_", " ", column)
+    ) +
+    # Aesthetics and theme
+    theme_classic() +
+    theme(
+      plot.title = element_text(size = 14, face = "bold"),
+      axis.title = element_text(size = 12),
+      axis.title.x = element_text(size = 10),
+      axis.text.y = element_blank(), # remove y values
+      axis.ticks.y = element_blank(),
+      panel.grid.major.x = element_line(color = "gray90", size = 0.5), # gridlines only on x axis
+      panel.grid.minor.x = element_line(color = "gray90", size = 0.2),
+    )
+  print(p)
+  
+  # Summary to console
+  cat("\nSummary Statistics for ", column, ":\n")
+  print(summary(data[[column]]))
+}
+
+for (column in numerical_columns_to_analyse) {
+  univariate_analysis_boxplot(data, column)
+}
+
 # =====================================
-# 
+# Univariate Analysis for categorical Columns
+# Distributions
 # =====================================
+
+# List of categorical columns for analysis
+categorical_columns_to_analyse <- c("person_gender", "person_education", "person_home_ownership",
+                                   "loan_intent", "previous_loan_defaults_on_file")
+
+# Function to plot distribution of categorical columns and percentage distribution
+plot_categorical_distribution <- function(data, column) {
+  # Bar plot
+  bar_plot <- ggplot(data, aes(x = factor(.data[[column]]), fill = factor(.data[[column]]))) +
+    geom_bar(
+      stat = "count",
+      color = "black"
+    ) +
+    geom_text(
+      aes(label = ..count..), 
+      stat = "count", 
+      vjust = -0.5, 
+      color = "black", 
+      size = 5
+    ) +
+    labs(
+      title = paste(gsub("_", " ", column), "Distribution"),
+      x = gsub("_", " ", column),
+      y = "Count"
+      ) +
+    scale_fill_brewer(palette = bar_palette_one) +
+    theme_classic() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+      plot.title = element_text(size = 14, face = "bold"),
+      axis.title = element_text(size = 12)
+      )
+  print(bar_plot)
+}
+ 
+for (column in categorical_columns_to_analyse) {
+  plot_categorical_distribution(data, column)
+  }
 
 # =====================================
 # 
